177698875c20bfc348130a5280d741a1f212f152
Sergio Coria <sergiocoria.my@gmail.com>
2024-12-26 02:02:40 -0400

    2

--- sergio1.py
+++ sergio1.py
@@ -1,2 +1,6 @@
 usuario1 = input ("dame un nombre: ")
 print (f"Bienvenido {usuario1} a este repositorio.. este es el primer ejercicio fecha de creacion diciembre 25 2024")
+
+duda = input ("ahora dime que deseas saber: ?")
+
+print (" ")
